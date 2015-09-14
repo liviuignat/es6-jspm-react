e52616828346b92c322d510fc3075dd5e81e4b5b
liviu.ignat <liviu.ignat@check24.de>
2015-09-14 15:25:35 +0200

    Router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { Router, Route } from 'react-router'
 import Home from './components/home';
 
 window.React = React;
 const mountNode = document.getElementById('app');
 
-React.render(<Home/>, mountNode);
+React.render((
+  <Router>
+    <Route path="/" component={Home}></Route>
+  </Router>
+), mountNode);
